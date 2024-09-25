6daa1268cb3bb13e2ff65553b95316d8953ff34a
John <jmikib@gmail.com>
2024-09-25 14:39:52 +0200

    Implemented PuT test for Room endpoint and GET & Delete for Client Endpoint

--- tests/testSuite.spec.ts
+++ tests/testSuite.spec.ts
@@ -79,6 +79,61 @@ import { test, expect } from '@playwright/test';
 
 })
 
+test('Test case 04 - Edit Room', async ({ request }) => {
+  const respCreatedRoom = await request.put("http://localhost:3000/api/room/2", {
+    headers: {
+      "Content-Type": "application/json", 
+      "X-user-auth": JSON.stringify({
+        username: "tester01",
+        token: tokenValue 
+      })
+    },
+    data: JSON.stringify({  // Convert the data to a JSON string
+      category: 'single',
+      floor: 3,
+      number: 777,
+      available: true,
+      price: 1669,
+      features: ['balcony', 'ensuite', 'penthouse']
+    })
+    
+  });
 
+  //console.log(await respCreatedRoom.json())
+  expect (await respCreatedRoom.ok())
+
+})
+
+test('Test case 05 - Get all clients', async ({ request }) => {
+  const respClients = await request.get("http://localhost:3000/api/clients", {
+    headers: {
+      "X-user-auth": JSON.stringify({
+        username: "tester01",
+        token: tokenValue 
+      })
+    },
+  });
+
+  console.log(await respClients.json())
+  expect (await respClients.ok())
+
+})
+
+test('Test case 06 - Delete client', async ({ request }) => {
+  const respDeletedRoom = await request.delete("http://localhost:3000/api/client/1", {
+    headers: {
+      "Content-Type": "application/json", 
+      "X-user-auth": JSON.stringify({
+        username: "tester01",
+        token: tokenValue 
+      })
+    },
+    
+  });
+
+  //console.log(await respCreatedRoom.json())
+  expect (await respDeletedRoom.ok())
+
+})
 
 })
